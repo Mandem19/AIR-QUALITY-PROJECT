6dc4d506f36e9ab99ddb7396f808fc464af2b9ec
Mandem19 <jeremydemirel98@gmail.com>
2022-12-05 17:02:09 +0100

    sensorBeginning

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,4 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class FileReader {
+
+
+    public static void main(String[] args) throws IOException {
+        String file = "lib\\data_10sensors_1year.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+
+        try {
+            reader = new BufferedReader(new java.io.FileReader(file));
+            while((line = reader.readLine()) != null){
+
+                String[] row = line.split(";");
+
+                for(String index: row){
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+
+            }
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+          
+        }
+        
+        /* 
+        finally{
+            try {
+                reader.close();
+            } catch (IOException e) {
+               
+                e.printStackTrace();
+            }
+
+        }
+        */
+        reader.close();
+
+    }
+
+
 
     
 }
