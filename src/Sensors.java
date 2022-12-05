2d3a9e101d030fb7f7655c952f01228e5f6bfed3
Mandem19 <jeremydemirel98@gmail.com>
2022-12-05 17:03:21 +0100

    bn

--- src/Sensors.java
+++ src/Sensors.java
@@ -1,3 +1,23 @@
 public class Sensors {
+
+    private String sensorID;
+    private double latitude; 
+    private double longitude; 
+    private String description; 
+
+
+    public Sensors(String sensID){
+        if(sensID.equalsIgnoreCase("Sensor0")){
+            this.sensorID="Sensor0";
+            this.latitude=-8.15758888291083;
+            this.longitude=-34.7692487876719;
+        }
+        if(sensID.equalsIgnoreCase("Sensor1")){
+            this.sensorID="Sensor1";
+            this.latitude=-30.0647387677174;
+            this.longitude=-76.3439147576429;
+        }
+
+    }
     
 }
